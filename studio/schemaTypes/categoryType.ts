ffdbe9f546b29ba464ed37d7d3e917ad6462b29d
Elie Kadoury <kadoury.elie@hotmail.com>
2025-04-05 17:51:37 -0400

    Added photos

--- studio/schemaTypes/categoryType.ts
+++ studio/schemaTypes/categoryType.ts
@@ -1,5 +1,5 @@
-import {FilterIcon} from '@sanity/icons'
-import {defineField, defineType} from 'sanity'
+import { FilterIcon } from '@sanity/icons'
+import { defineField, defineType } from 'sanity'
 
 export const categoryType = defineType({
   name: 'category',
@@ -7,13 +7,15 @@ export const categoryType = defineType({
   type: 'document',
   icon: FilterIcon,
   fields: [
-    defineField({name: 'name', type: 'string'}),
-    defineField({name: 'slug', type: 'slug'}),
+    defineField({ name: 'name', type: 'string' }),
+    defineField({ name: 'slug', type: 'slug' }),
+    defineField({ name: 'featuredMedia', type: 'image' })
   ],
   preview: {
     select: {
       title: 'name',
       subtitle: 'slug.current',
+      media: 'featuredMedia',
     },
   },
 });
