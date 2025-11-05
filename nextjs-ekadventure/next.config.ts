7444f6b37d629218aa1edc9064ec54c907a1d3be
Elie Kadoury <kadoury.elie@hotmail.com>
2025-11-05 15:42:33 -0500

    Updated dependencies to the latest stable versions

--- nextjs-ekadventure/next.config.ts
+++ nextjs-ekadventure/next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["cdn.sanity.io"],
+    domains: ["cdn.sanity.io", "r2.ekadventure.com", "i0.wp.com"],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'ekadventure.com', pathname: '/cdn-cgi/image/**' }
+    ]
   }
 };
 
