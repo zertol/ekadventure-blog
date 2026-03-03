230065b201dfc8f397571b1fed3d1f556412deb5
Elie Kadoury <kadoury.elie@hotmail.com>
2026-03-03 11:42:23 -0500

    Search Results from Submit now in a modal view + Handle global outside locale not found page

--- nextjs-ekadventure/src/components/UI/Common/SearchResults/SearchResult.tsx
+++ nextjs-ekadventure/src/components/UI/Common/SearchResults/SearchResult.tsx
@@ -2,15 +2,20 @@ import { Link } from "@/i18n/navigation";
 import { SearchResultType } from "@/types/search-result-type";
 import { escapeHtml, highlightTerm } from "@/utils/data/helpers";
 import { useTranslations } from "next-intl";
+import NextLink from "next/link";
 
 const SearchResult: React.FC<{
   result: SearchResultType;
   highlight: string;
-}> = ({ result, highlight }) => {
-
-  const tUI = useTranslations("UI");
-
-  const label = result._type === "category" ? tUI("searchResultCategoryHeader") : tUI("searchResultPostHeader");
+  outsideLocale?: boolean;
+}> = ({ result, highlight, outsideLocale }) => {
+  const label = outsideLocale
+    ? result._type === "category"
+      ? "Category"
+      : "Post"
+    : result._type === "category"
+      ? useTranslations("UI")("searchResultCategoryHeader")
+      : useTranslations("UI")("searchResultPostHeader");
 
   const titleHtml = highlight
     ? highlightTerm(result.title || result.name, highlight)
@@ -23,25 +28,33 @@ const SearchResult: React.FC<{
 
   return (
     <div className="p-3 rounded">
-    <div className="flex-left-col mb-2">
-        <span className="text-xs p-0 px-3 bg-background-green-accent rounded-sm text-white">{label}</span>
-    </div>
+      <div className="flex-left-col mb-2">
+        <span className="text-xs p-0 px-3 bg-background-green-accent rounded-sm text-white">
+          {label}
+        </span>
+      </div>
       <div className="flex items-baseline justify-between">
-        
-        <Link
-          href={result.href || "#"}
-          className="font-semibold text-xs text-white underline hover:text-background-green-accent"
-          dangerouslySetInnerHTML={{ __html: titleHtml }}
-        />
-        </div>
-      
+        {outsideLocale ? (
+          <NextLink
+            href={result.href || "#"}
+            className="font-semibold text-xs text-white underline hover:text-background-green-accent"
+            dangerouslySetInnerHTML={{ __html: titleHtml }}
+          />
+        ) : (
+          <Link
+            href={result.href || "#"}
+            className="font-semibold text-xs text-white underline hover:text-background-green-accent"
+            dangerouslySetInnerHTML={{ __html: titleHtml }}
+          />
+        )}
+      </div>
+
       {snippetHtml && (
         <p
           className="mt-1 text-white text-xs"
           dangerouslySetInnerHTML={{ __html: snippetHtml }}
         />
       )}
-      
     </div>
   );
 };
